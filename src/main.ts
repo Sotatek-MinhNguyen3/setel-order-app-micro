d26b2ed6c32d504442009b157492d7c88ae42553
Minh Nguyen <minh.nguyen3@sotatek.com>
2021-02-23 22:33:27 +0700

    try to fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ require('dotenv').config({ path: `../${process.env.NODE_ENV.trim()}.env` });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
